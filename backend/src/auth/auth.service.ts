283fd6da59f2dc53e7b39cfeb65e01149a1efe2f
umar006 <umaralfaruq597@gmail.com>
2024-05-08 14:25:45 +0700

    feat: login service

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -25,6 +26,28 @@ export class AuthService {
       sub: user.id,
       name: user.name,
       email: user.email,
+    };
+
+    const accessToken = await this.jwtService.signAsync(payload);
+
+    return { accessToken };
+  }
+
+  async login(body: LoginDto) {
+    const user = await this.usersService.getUserByEmailWithPassword(body.email);
+    if (!user) {
+      throw new UnauthorizedException('email or password is wrong, bro');
+    }
+
+    const isValidated = await bcrypt.compare(body.password, user.password);
+    if (!isValidated) {
+      throw new UnauthorizedException('email or password is wrong, bro');
+    }
+
+    const payload = {
+      sub: user.id,
+      email: user.email,
+      name: user.name,
     };
 
     const accessToken = await this.jwtService.signAsync(payload);
