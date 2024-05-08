876de90d23f693c564cafce3aaab11376fa12dea
umar006 <umaralfaruq597@gmail.com>
2024-05-08 15:50:12 +0700

    fix: extract jwt token

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -16,10 +16,11 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  validate(payload: { sub: string; username: string }) {
+  validate(payload: { sub: string; email: string; name: string }) {
     return {
       id: payload.sub,
-      username: payload.username,
+      email: payload.email,
+      name: payload.name,
     };
   }
 }
