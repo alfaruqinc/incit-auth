c07e559ed0462c66033fcc899de43fb58885f951
umar006 <umaralfaruq597@gmail.com>
2024-05-08 13:46:45 +0700

    feat: auth controller

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async signUp(@Body() registerDto: CreateUserDto) {
+    return await this.authService.signUp(registerDto);
+  }
+}
