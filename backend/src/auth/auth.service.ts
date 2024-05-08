bc7f2bdd7c25c45349d8c224568c7edf3c8b4fec
umar006 <umaralfaruq597@gmail.com>
2024-05-08 14:07:16 +0700

    feat: return access token after register

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async signUp(body: CreateUserDto) {
     const saltOrRounds = 10;
@@ -15,6 +19,16 @@ export class AuthService {
       password: hashedPassword,
     };
 
-    await this.usersService.createUser(newUser);
+    const user = await this.usersService.createUser(newUser);
+
+    const payload = {
+      sub: user.id,
+      name: user.name,
+      email: user.email,
+    };
+
+    const accessToken = await this.jwtService.signAsync(payload);
+
+    return { accessToken };
   }
 }
