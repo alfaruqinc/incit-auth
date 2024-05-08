b5ad8a59ddf182018c92fa1e382e1ce0b73daa0a
umar006 <umaralfaruq597@gmail.com>
2024-05-08 16:55:17 +0700

    remove: unnecessary code

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -1,25 +1,32 @@
 import { Inject } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import authConfig from 'src/config/auth.config';
 import { JwtPayload } from './jwt-payload.interface';
-import { UsersService } from 'src/users/users.service';
 
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
-    private readonly userSerivce: UsersService,
     @Inject(authConfig.KEY)
     private authCfg: ConfigType<typeof authConfig>,
   ) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          return request.cookies.Authentication;
+        },
+      ]),
       secretOrKey: authCfg.JWT_SECRET,
       ignoreExpiration: false,
     });
   }
 
   async validate(payload: JwtPayload) {
-    return await this.userSerivce.getUserById(payload.sub);
+    return {
+      id: payload.sub,
+      email: payload.email,
+      name: payload.name,
+    };
   }
 }
