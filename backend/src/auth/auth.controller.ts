3922eee01873777c9c76ca1d85ae17ce27d1ca2c
umar006 <umaralfaruq597@gmail.com>
2024-05-08 14:25:57 +0700

    feat: login api

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 import { Public } from './public.decorator';
 
 @Controller('auth')
@@ -11,5 +12,11 @@ export class AuthController {
   @Post('register')
   async register(@Body() registerDto: CreateUserDto) {
     return await this.authService.register(registerDto);
+  }
+
+  @Public()
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    return await this.authService.login(loginDto);
   }
 }
