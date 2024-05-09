ac88ecaf2e99a526f50144ee2cae27e1d2bfc3ce
umar006 <umaralfaruq597@gmail.com>
2024-05-09 10:45:55 +0700

    feat: login form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
+import { FormEvent, useState } from "react";
+
 function App() {
+  const [show, setShow] = useState("");
+
   const authBtn = () => {
     return (
       <div>
-        <button>login</button>
-        <button>register</button>
+        <button onClick={() => setShow("login")}>login</button>
+        <button onClick={() => setShow("register")}>register</button>
       </div>
     );
   };
@@ -12,8 +16,51 @@ function App() {
     <>
       <h1>Auth</h1>
       {authBtn()}
+      {show === "login" && <Login />}
     </>
   );
 }
 
 export default App;
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const loginForm = () => {
+    return (
+      <div>
+        <h1>LOGIN</h1>
+        <form onSubmit={handleLogin}>
+          <label>
+            <span>email</span>
+            <input
+              type="text"
+              value={email}
+              placeholder="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <br />
+          <label>
+            <span>password</span>
+            <input
+              type="password"
+              value={password}
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <br />
+          <button type="submit">login</button>
+        </form>
+      </div>
+    );
+  };
+
+  return loginForm();
+};
