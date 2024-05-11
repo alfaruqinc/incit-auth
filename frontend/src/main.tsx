fa871be5a60dd283084d93ba9e67178a6fc793da
umar006 <umaralfaruq597@gmail.com>
2024-05-11 09:02:36 +0700

    feat: setup react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,19 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Root } from "./routes/root.tsx";
 
 const queryClient = new QueryClient();
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <RouterProvider router={router} />
   </QueryClientProvider>,
 );
